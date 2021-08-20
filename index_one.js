4ba48ff5d9630fba2ae793433ce5e59c49206124
amirali1412 <amirali14@hotmail.co.uk>
2021-08-20 16:18:25 +0000

    Add words to level medium

--- index_one.js
+++ index_one.js
@@ -1,3 +1,3 @@
 words_easy = ["happy", "table", "books", "extra", "queen", "apple", "fairy", "zebra", "prince", "joker"]
-words_medium = []
+words_medium = ["sizzle", "archery", "spider", "joyful", "cheque", "zombie", "duplex", "unwind", "object", "mumble"]
 words_hard = []
