6cdf344dc0ef8f97d9b67dc3a054382deeb5307d
amirali1412 <amirali14@hotmail.co.uk>
2021-08-20 17:05:58 +0000

    While loop made

--- index_one.js
+++ index_one.js
@@ -1,3 +1,3 @@
 words_easy = ["happy", "table", "books", "extra", "queen", "apple", "fairy", "zebra", "prince", "joker"]
 words_medium = ["sizzle", "archery", "spider", "joyful", "cheque", "zombie", "duplex", "unwind", "object", "mumble"]
-words_hard = []
+words_hard = ["jacuzzi", "kitchen", "graphic", "invests", "lecture", "nurture", "oxidize", "rejoice", "toolbox", "wizards"]
--- run.py
+++ run.py
@@ -1,3 +1,21 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+words_easy = ["happy", "table", "books", "extra", "queen", "apple", "fairy", "yacht", "prince", "joker"]
+
+lives = 7
+guesses []
+done = False
+
+while not done:
+    for letter in word:
+        if letter.upper() in guesses:
+            print(letter, end=" ")
+        else:
+            print("_", end=" ")
+    print("")
+    done=True
+
+guess = input(f"Lives Left {lives}, Next Guess: ")
+
